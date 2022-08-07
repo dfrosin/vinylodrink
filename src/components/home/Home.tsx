c0f8821e44e5e9679c5f33bee8b09c0492c0830d
David Forsyth Rosin <drosin96@gmail.com>
2022-08-07 19:14:58 +0200

    add initial functions and homepage

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,15 @@
+import './home.css'
+import { fetchAlbum, fetchDrink } from '../../utils'
+
+function Home() {
+  console.log(fetchAlbum())
+  return (
+    <div className="Home">
+      <main>
+        <p>hejhej</p>
+      </main>
+    </div>
+  )
+}
+
+export default Home
