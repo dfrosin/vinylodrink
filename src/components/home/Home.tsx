789f600a649301d361b2d88d2b1355d0d16bb06b
David Forsyth Rosin <drosin96@gmail.com>
2022-08-27 22:31:21 +0200

    better looking animation

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,19 +1,19 @@
-import './home.css'
-import { fetchAlbum, fetchDrink } from '../../utils'
-import Vinyl from './vinylprototype/Vinyl'
+import './home.css';
+import { fetchAlbum, fetchDrink } from '../../utils';
+import Vinyl from './vinylprototype/Vinyl';
 
 function Home() {
-  console.log(fetchDrink())
-  console.log(fetchAlbum())
+  console.log(fetchDrink());
+  console.log(fetchAlbum());
   return (
     <div className="Home">
       <main>
         <h1>Vinyl & Drink</h1>
-        <img src="/src/assets/vinylLogo.svg" alt="vinyldrink logo" />
+        {/* <img src="/src/assets/vinylLogo.svg" alt="vinyldrink logo" /> */}
         <Vinyl />
       </main>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/components/home/vinylprototype/Vinyl.tsx
+++ src/components/home/vinylprototype/Vinyl.tsx
@@ -1,16 +1,36 @@
-import './vinyl.css'
+import { useState } from 'react';
+import './vinyl.css';
 
 function Vinyl() {
+  const [isClicked, setClicked] = useState<boolean>(false);
+  const [isVisible, setVisible] = useState<boolean>(true);
+  const clicked = () => {
+    setClicked(true);
+    setTimeout(() => setVisible(false), 5000);
+  };
   return (
-    <div className="Vinyl">
-      <img className="drink" src="/drink.svg" alt="" />
-      <div className="inside-vinyl"></div>
-      <div className="both-lines">
-        <div className="line"></div>
-        <div className="line2"></div>
-      </div>
-    </div>
-  )
+    <>
+      {isVisible && (
+        <div
+          onClick={clicked}
+          className={`Vinyl${isClicked ? ' clicked' : ''}`}
+        >
+          <div className="vinyl-center">
+            <img className="drink" src="/drink.svg" alt="" />
+          </div>
+          <div className="inside-vinyl"></div>
+          <div className="both-lines">
+            <div className="line"></div>
+            <div className="line2"></div>
+          </div>
+          <div className="both-lines2">
+            <div className="line"></div>
+            <div className="line2"></div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Vinyl
+export default Vinyl;
